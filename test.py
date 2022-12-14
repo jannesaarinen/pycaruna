7ed168a61eb15990462e31dd974b6a88cb8ded41
Janne Saarinen <janne@saariset.org>
2022-12-14 15:47:06 +0200

    job interval increase

---
+++ test.py
@@ -0,0 +1,44 @@
+import time
+import sys
+import logging
+
+from caruna_integration import get_hourly_measurements
+from caruna_integration.config import config
+
+# from timeloop import Timeloop
+#from datetime import timedelta
+
+# Get configuration
+params = config(section='main')
+
+# Initialize new timeloop object
+#tl = Timeloop()
+
+# Setup logging
+logger = logging.getLogger('main')
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.DEBUG)
+
+# Timeloop function
+# @tl.job(interval=timedelta(seconds=int(params['job_interval'])))
+# def periodic_job():
+#     logger.info("{}: starting caruna_integration.get_hourly_measurements()".format(time.ctime()))
+#     try:
+#         get_hourly_measurements.get_hourly_measurements(int(params['job_interval']))
+#     except (Exception) as error:
+#         logger.error("caruna_integration.get_hourly_measurements() returned an error:")
+#         logger.error(error)
+#         pass
+
+# tl.start(block=True)
+
+try:
+    get_hourly_measurements.get_hourly_measurements(int(params['job_interval']))
+except (Exception) as error:
+    logger.error("caruna_integration.get_hourly_measurements() returned an error:")
+    logger.error(error)
+    pass
